430a25b154f7792fa622ffd6ecf75f193a4f9996
BINEESH MATHEW <108644842+Ryuk38@users.noreply.github.com>
2025-03-01 01:28:02 +0530

    Add files via upload

---
+++ dashboard.php
@@ -0,0 +1,159 @@
+<?php
+session_start();
+if (!isset($_SESSION['id'])) {
+  echo '<script>alert("Please Login to Continue"); window.location.href="login.php";</script>';
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>LMS | Dashboard</title>
+  <style>
+    body {
+      background-image: url("images/open-bg (2).jpg");
+      background-size: cover;
+      background-position: center;
+      font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+      margin: 0;
+      padding: 0;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      min-height: 100vh;
+      color: white;
+    }
+
+    .dashboard-container {
+      background: rgba(0, 0, 0, 0.7);
+      padding: 40px;
+      border-radius: 10px;
+      width: 50%;
+      max-width: 800px;
+      box-shadow: 0 4px 8px rgba(0, 0, 0, 0.2);
+      text-align: center;
+    }
+
+    .dashboard-container h2 {
+      font-size: 30px;
+      margin-bottom: 20px;
+    }
+
+    .dashboard-container p {
+      margin-bottom: 30px;
+      font-size: 18px;
+    }
+
+    .dashboard-container a {
+      display: block;
+      margin-bottom: 15px;
+      padding: 12px 24px;
+      background-color: #ff4500;
+      color: white;
+      text-decoration: none;
+      border-radius: 4px;
+      transition: background-color 0.3s ease;
+    }
+
+    .dashboard-container a:hover {
+      background-color: #ff6347;
+    }
+  </style>
+</head>
+
+<body>
+  <div class="dashboard-container">
+    <?php
+    $servername = "localhost";
+    $username = "root";
+    $password = "";
+    $dbname = "cms";
+
+    $id = $_SESSION['id'];
+
+    $conn = new mysqli($servername, $username, $password, $dbname);
+
+    if ($conn->connect_error) {
+      die("Connection failed: " . $conn->connect_error);
+    } else {
+
+      $user_sql = "SELECT user_type FROM login WHERE id=$id";
+      $result = $conn->query($user_sql);
+      if ($result->num_rows > 0) {
+        $row = $result->fetch_assoc();
+        //Students
+        if ($row['user_type'] === '1') {
+          $sql = "SELECT student_info.First_Name FROM student_info JOIN login ON login.id = student_info.stud_id WHERE stud_id=$id";
+
+          $result = $conn->query($sql);
+          if ($result->num_rows > 0) {
+            $row = $result->fetch_assoc();
+            echo "<h2>Welcome to Your Dashboard, " . $row["First_Name"] . "!</h2>";
+            echo '<p>This is where you can manage your courses:</p>
+            <a href="course.php">View Courses</a>
+            <a href="enrolled_course.php">Enrolled Courses</a>
+            <a href="update_profile.php">Edit Profile</a>
+            <a href="change_pass.php">Change Password</a>
+            <a href="logout.php">Logout</a>';
+          } else {
+            echo "<h2>Welcome to Your Dashboard </h2>";
+            echo '<p>This is where you can manage your courses:</p>
+            <a href="course.php">View Courses</a>
+            <a href="enrolled_course.php">Enrolled Courses</a>
+            <a href="update_profile.php">Edit Profile</a>
+            <a href="change_pass.php">Change Password</a>
+            <a href="logout.php">Logout</a>';
+          }
+        }
+        //teachers
+
+        elseif ($row['user_type'] === '2') {
+          $sql = "SELECT teacher_info.First_Name FROM teacher_info JOIN login ON login.id = teacher_info.tr_id WHERE tr_id=$id";
+          $result = $conn->query($sql);
+          if ($result->num_rows > 0) {
+            $row = $result->fetch_assoc();
+            echo "<h2>Welcome to Your Dashboard, " . $row["First_Name"] . "!</h2>";
+            echo '<a href="course.php">View Courses</a>';
+            echo '<a href="enrolled_course.php">Enrolled Students</a>';
+
+            $course_id_sql = "SELECT a.course_id FROM course a JOIN login b ON a.tr_id=b.id where tr_id=$id";
+            $result = $conn->query($course_id_sql);
+            if ($result->num_rows > 0) {
+              $row = $result->fetch_assoc();
+              echo '<a href="lesson.php?course_id=' . urlencode($row['course_id']) . '">Lessons</a>';
+              echo '<a href="update_profile.php">Edit Profile</a>';
+              echo '<a href="change_pass.php">Change Password</a>';
+              echo '<a href="logout.php">Logout</a>';
+            }
+          } 
+          else {
+            echo "<h2>Welcome to Your Dashboard </h2>";
+            echo '<p>This is where you can manage your courses:</p>
+            <a href="course.php">View Courses</a>
+            <a href="enrolled_course.php">Enrolled Courses</a>
+            <a href="update_profile.php">Edit Profile</a>
+            <a href="change_pass.php">Change Password</a>
+            <a href="logout.php">Logout</a>';
+          }
+        } else
+        //admin
+        {
+          echo "<h2>Welcome to Your Admin Panel</h2><br>";
+          echo '
+            <a href="admin_panel.php">Courses</a>
+            <a href="course.php">View Courses</a>
+            <a href="teachers.php">Assign Teachers</a>
+            <a href="change_pass.php">Change Password</a>
+            <a href="logout.php">Logout</a>';
+        }
+      }
+    }
+    ?>
+
+  </div>
+</body>
+
+</html>
---
+++ register.php
@@ -0,0 +1,150 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "cms";
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    if (isset($_POST['username']) && isset($_POST['password'])) {
+        $usnm = $_POST['username'];
+        $psswd = $_POST['password'];
+        $user_type = 1;
+
+        $conn = new mysqli($servername, $username, $password, $dbname);
+
+        if ($conn->connect_error) {
+            die("Connection failed: " . $conn->connect_error);
+        } else {
+
+            $check = "SELECT username FROM login where username='$usnm'";
+            $result = $conn->query($check);
+            if ($result->num_rows > 0) {
+                echo '<script>alert("Username already exists. Choose a different username!!!"); window.location.href="register.php";</script>';
+            } else {
+                $sql = $conn->prepare("INSERT INTO login(username,pass,user_type)  VALUES (?, ?, ?)");
+                $sql->bind_param("ssi", $usnm, $psswd, $user_type);
+                if ($sql->execute()) {
+                    echo '<script>alert("Registration successful."); window.location.href="login.php";</script>';
+                } else {
+                    echo '<script>alert("Error: ' . $stmt->error . '"); window.location.href="register.php";</script>';
+                }
+            }
+        }
+    }
+}
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>LMS | Register</title>
+    <style>
+        body {
+            background-image: url("images/open-bg (2).jpg");
+            background-size: cover;
+            background-position: center;
+            font-family: system-ui, -apple-system, BlinkMacSystemFont, "Segoe UI",
+                Roboto, Oxygen, Ubuntu, Cantarell, "Open Sans", "Helvetica Neue",
+                sans-serif;
+            margin: 0;
+            padding: 0;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            height: 100vh;
+            color: white;
+        }
+
+        .register-container {
+            background: rgba(0, 0, 0, 0.7);
+            /* Semi-transparent background */
+            padding: 50px;
+            border-radius: 10px;
+            width: 400px;
+            box-shadow: 0 4px 8px rgba(0, 0, 0, 0.2);
+        }
+
+        .register-container h2 {
+            text-align: center;
+            font-size: 35px;
+            margin-bottom: 40px;
+        }
+
+        .register-container label {
+            display: block;
+            margin-top: 20px;
+            font-size: 18px;
+        }
+
+        .register-container input[type="text"],
+        .register-container input[type="password"] {
+            width: 95%;
+            padding: 12px;
+            margin-top: 10px;
+            border: none;
+            border-radius: 5px;
+            background: rgba(255, 255, 255, 0.1);
+            color: white;
+            font-size: 16px;
+        }
+
+        .register-container input::placeholder {
+            color: rgba(255, 255, 255, 0.7);
+        }
+
+        .register-container input:focus {
+            outline: none;
+            background: rgba(255, 255, 255, 0.2);
+        }
+
+        .register-container button {
+            width: 100%;
+            padding: 12px;
+            margin-top: 30px;
+            border: none;
+            border-radius: 5px;
+            background-color: #ff4500;
+            /* Orange color */
+            color: white;
+            font-size: 20px;
+            cursor: pointer;
+        }
+
+        .register-container button:hover {
+            background-color: #ff6347;
+            /* Lighter orange */
+        }
+
+        .register-container p {
+            text-align: center;
+            margin-top: 30px;
+            font-size: 18px;
+        }
+
+        .register-container a {
+            color: white;
+            text-decoration: none;
+        }
+    </style>
+</head>
+
+<body>
+    <div class="register-container">
+        <h2>Register</h2>
+        <form action="register.php" method="post">
+            <label for="username">Username</label>
+            <input type="text" id="username" name="username" placeholder="Type your username" required />
+            <label for="password">Password</label>
+            <input type="password" id="password" name="password" placeholder="Type your password" required />
+
+            <button type="submit">Register</button>
+        </form>
+        <a href="login.php">
+            <p style="margin-top: 30px">Already have an account? Login</p>
+        </a>
+    </div>
+</body>
+
+</html>
