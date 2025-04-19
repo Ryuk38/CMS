cfe5783443648904dab10721e79373565280b91f
BINEESH MATHEW <108644842+Ryuk38@users.noreply.github.com>
2025-04-19 15:06:21 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,9 +1,9 @@
 <?php
-$host = 'host.docker.internal';  // The hostname or IP address of your database server
-$db = 'cms';                     // The name of your database
-$user = 'root';                  // Your MySQL username
-$pass = 'root';                  // Your MySQL password
-$port = 3307;                    // The MySQL server port
+$host = '172.17.0.1';  // IP address of your host machine on Docker network
+$db = 'cms';            // Your database name
+$user = 'root';         // Your MySQL username
+$pass = 'root';         // Your MySQL password
+$port = 3307;           // MySQL port
 
 // Create connection using the defined variables
 $conn = new mysqli($host, $user, $pass, $db, $port);
