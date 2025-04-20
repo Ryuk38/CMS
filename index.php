c4203cc0e4eeb42128b6caf404742a8b14c55c8d
BINEESH MATHEW <108644842+Ryuk38@users.noreply.github.com>
2025-04-20 22:42:21 +0530

    Update index.php

--- index.php
+++ index.php
@@ -3,7 +3,7 @@ $host = "mysql";        // Use the MySQL container name
 $user = "root";         // MySQL username
 $pass = "root";         // MySQL password
 $db   = "cms";          // Name of your DB (you can check it in phpMyAdmin)
-$port = 3307;           // Default MySQL port
+$port = 3306;           // Default MySQL port
 
 $conn = new mysqli($host, $user, $pass, $db, $port);
 
