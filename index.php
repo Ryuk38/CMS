f194872b6cb7dd789636154228903fd8fadae692
Ryuk38 <c380bineesh@gmail.com>
2025-04-20 23:19:44 +0530

    success

--- index.php
+++ index.php
@@ -11,5 +11,5 @@ $conn = new mysqli($host, $user, $pass, $db, $port);
 if ($conn->connect_error) {
     die("❌ Connection failed: " . $conn->connect_error);
 }
-echo "✅ Successfully connected to MySQL from PHP container!";
+echo "✅ Successfully connected to MySql";
 ?>
